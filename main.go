0508a1d24118d58adb227fb942ad4a3c4a72ecf6
Kelden <ongkelden@easyparcel.com>
2022-11-23 15:51:43 +0800

    feat: Data types

--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remaindingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remaindingTickets uint = 50
 
 	// fmt.Println("Welcome to", conferenceName, "application")
 	// fmt.Println("Total of", conferenceTickets, "and", remaindingTickets, "are available")
 	fmt.Printf("Welcome to %v application \n", conferenceName)
 	fmt.Printf("Total of %v and %v are available \n", conferenceTickets, remaindingTickets)
 	fmt.Println("Get your booking to attend")
+
+	// print data type
+	fmt.Printf("conferenceTickets is %T, remaintingTickets is %T, conferenceName is %T \n", conferenceTickets, remaindingTickets, conferenceName)
+
+	// Declaring data type
+	var userName string
+	var userTickets int
+
+	userName = "Alex"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets \n", userName, userTickets)
 
 }
